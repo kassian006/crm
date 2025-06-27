66f97300897ce54809d35ff0d91aac8b1525bfb8
Argen Joldoshaliev <argenjoldoshaliev006@gmail.com>
2025-06-27 16:50:41 +0600

    Im finish my part

--- mysite/crm_app/views.py
+++ mysite/crm_app/views.py
@@ -183,12 +183,34 @@ class DoctorViewSet(viewsets.ModelViewSet):
         return Doctor.objects.filter(pk=self.request.user.pk)
 
 
-class DoctorListViewSet(viewsets.ModelViewSet):
+class DoctorListAPIView(generics.ListAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorListSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['department']
     search_fields = ['first_name', 'last_name']
+
+
+class DoctorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorDetailSerializer
+
+
+class DoctorCreateAPIView(generics.CreateAPIView):
+    serializer_class = DoctorCreateSerializer
+
+    # def create(self, request, *args, **kwargs):
+    #     try:
+    #         serializer = self.get_serializer(data=request.data)
+    #         serializer.is_valid(raise_exception=True)
+    #         doctor = serializer.save()
+    #         return Response(serializer.data, status.HTTP_201_CREATED)
+    #     except serializers.ValidationError as e:
+    #         return Response({'detail': 'Маалымат туура эмес берилди'}, status.HTTP_400_BAD_REQUEST)
+    #     except NameError as e:
+    #         return Response({'detail': f'{e}, Ошибка в коде'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     except Exception:
+    #         return Response({'detail': 'Сервер не работает'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class DoctorServicesViewSet(viewsets.ModelViewSet):
